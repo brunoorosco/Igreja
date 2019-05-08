cf5ed480fdf17ce39181198e471fb22be4536d9c
brunoorosco <brunoorosco@GMAIL.COM>
2019-05-08 16:58:14 -0300

    ajustes 08/05

--- app/curso/ler_turma.php
+++ app/curso/ler_turma.php
@@ -6,21 +6,20 @@
     
 
     //Conectando ao banco de dados
-    $con = new mysqli("localhost", "root", "", "db_igreja");
-    if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
-
-    $idCursos = $_POST['curso'];
+     $idCursos = $_POST['curso'];
 // query para quem participa do curso
     $sql = "SELECT infocursos.nomeCursos, membros.nome, membros.idmembros	FROM turma INNER JOIN membros
     ON turma.alunos = membros.idmembros INNER JOIN infocursos
     ON turma.curso = infocursos.idCursos WHERE infocursos.idCursos = $idCursos ORDER BY membros.nome ASC";
     
     //Consultando banco de dados
-    $qryLista = mysqli_query($con, $sql);    
-    while($resultado = mysqli_fetch_assoc($qryLista)){
-        $vetor[] = array_map('utf8_encode', $resultado); 
-        
-    }   
+    
+    $db = new db();
+    $db = $db->connect();
+    foreach($db->query($sql) as $row)
+        {
+            $vetor[] = $row;
+        }
     
     //Passando vetor em forma de json
     echo json_encode($vetor);
--- app/curso/turmas.php
+++ app/curso/turmas.php
@@ -6,12 +6,13 @@
       
 
     //Conectando ao banco de dados
-    $con = new mysqli("localhost", "root", "", "db_igreja");
-    if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
-
-     $idCursos = $_POST['curso'];
+   // $con = new mysqli("localhost", "root", "", "db_igreja");
+    //if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
     
-  /*  $sql = "SELECT membros.nome	FROM turma INNER JOIN membros
+     $idCursos = $_POST['curso'];
+     $data=array();
+        
+     /*  $sql = "SELECT membros.nome	FROM turma INNER JOIN membros
            ON turma.alunos = membros.idmembros INNER JOIN infocursos
            ON turma.curso = infocursos.idCursos WHERE infocursos.idCursos like $idCursos";
 */
@@ -19,14 +20,22 @@
         where idmembros not in (select alunos
         from turma where curso='$idCursos') ORDER BY nome ASC" ;     
     
+        $db = new db();
+        $db = $db->connect();
+        foreach($db->query($sql) as $row)
+            {
+                $data[] = $row;
+            }
+
+
     //Consultando banco de dados
-    $qryLista = mysqli_query($con, $sql);    
-    while($resultado = mysqli_fetch_assoc($qryLista)){
-        $vetor[] = array_map('utf8_encode', $resultado); 
-        }    
+    //$qryLista = mysqli_query($con, $sql);    
+    //while($resultado = mysqli_fetch_assoc($qryLista)){
+    //    $vetor[] = array_map('utf8_encode', $resultado); 
+     //   }    
     
     //Passando vetor em forma de json
-    echo json_encode($vetor);
+    echo json_encode($data);
     //var_dump (json_encode($vetor));
    
 ?>
