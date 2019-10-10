e7cb2a61f54f07e8afdcde9db2d011660e66c18b
brunoorosco <brunoorosco@gmail.com>
2019-10-10 16:59:31 -0300

    ajuestes 10-10

--- app/encontro/cadastroEncontro.php
+++ app/encontro/cadastroEncontro.php
@@ -1,13 +1,15 @@
 <?php
-
-	include_once("../../_fonts/config/banco.php");
-  include_once "../../menu.php";
+ include_once("../../_fonts/config/funcoes.php");
+ include_once('../../_fonts/config/banco.php');
+ include_once("../../menu.php");
+ 
+ validarUsuario();
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-		<<meta charset="utf-8">
+		<meta charset="utf-8">
 		<meta name="description" content="Secretária Virtual da Comunidade Avivamento em Cristo">
 		<meta name="keywords" content="Secretária Virtual">
 		<meta name="author" content="Comunidade Avivamento em Cristo">
@@ -16,96 +18,9 @@
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-		<title>Inscrição de Encontristas</title>
-
-		<link rel="stylesheet" type="text/css" href="_fonts/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="_fonts/css/sweetalert2.min.css">
-
-
-		<script type="text/javascript" src="_fonts/js/bootstrap.js"></script>
-		<script type="text/javascript" src="_fonts/js/jquery.mask.js"></script>
-
-    <style type="text/css">
-	  * {
-	  box-sizing: border-box;
-	  margin: 0;
-	 padding: 0;
-	}
-
-    html, body{height:100%;}
-
-      body{
-        background-image: url(img/efata.jpg);
-        background-size: cover;
-        color: white;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-attachment: fixed;
-
-
-      }
-      form{
-        background: rgba(45, 45, 125, 0.55);
-        color: white;
-        padding: 40px;
-        margin-top: 150px;
-        padding-bottom: 60px;
-        box-shadow: 10px 10px 5px rgba(6, 1, 1, 0.50);
-
-
-
-      }
-
-      @media(min-width:700px) {
-      	 form{
-             width: 400px;
-             margin-top: 75px;
-             margin-left: 10%;
-
-      }
-     }
-
-     @media(max-width:680px) {
-      	 form{
-             width: 400px;
-             margin-top: 50px;
-             margin-left: -5%;
-
-      }
-     }
-
-      h1{
-        text-align: center;
-      }
-      .btn{
-        width: 100%;
-        border-radius: 0px;
-
-      }
-      .form-control{
-        border-radius: 0px;
-        background-color: rgba(23, 3, 3, 0.48);
-        color: white;
-        
-				
-				
-				
-				
-				border-radius:1px solid #291212
-       }
-
-	     .form-signin{
-		width: 100%;
-		max-width: 600px;
-		padding: 15px;
-		margin:auto;
-			}
-    </style>
-    <script type="text/javascript">
-		mascara();
-		 </script>
-
-	</head>
+		<title>Cadastro de Encontro</title>
+	
+</head>
 <body>
 
        <div class="container">
@@ -113,7 +28,7 @@
        		<div class="row"></div>
        		<div class="col-md-6 offset-md-3">
 			<form  id="" action="cadDB.php" method="post" >
-				<h3>Inscrição de Encontristas</h3><br>
+				<h3>Cadastro de Encontro</h3><br>
 				 <div class="form-group ">
 				 	<input type="text" name="nome" class="form-control" placeholder="Nome Completo" required="">
 				 </div>
