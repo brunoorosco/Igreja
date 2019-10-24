cae72058886ef156b00268f4c165073c85e68e24
brunoorosco <brunoorosco@gmail.com>
2019-10-24 19:22:42 -0300

    ajustes 24/10

--- printBatismo.php
+++ printBatismo.php
@@ -1,6 +1,7 @@
 <?php
 //include("mpdf60/mpdf.php");
 //include_once("_fonts/ funcoes.php");
+header('Content-Type: text/html; charset=utf-8');
 require('_fonts/fpdf/fpdf.php');
 
 //Connect to your database
@@ -8,22 +9,29 @@ require('_fonts/fpdf/fpdf.php');
 // Connect to database...
 
 
+
+$str = date('DD', strtotime('+day')). ' de ' . date('Monthy', strtotime('+month'));
+//resultado: Mar
+
 $pdf = new FPDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();              
 $pdf->AddFont('Pacifico','','Pacifico.php');
 $pdf->SetFont('Pacifico', '', 24);
-$pdf->SetTextColor(20, 20, 100); //rGB
+$pdf->SetTextColor(255,140,0); //rGB
 
 //$pdf = new FPDF();
 //$pdf->AddPage();
 //$pdf->Image('_fonts/img/batismo.png', 0, 0, $fpdf->w, $fpdf->h);
 $pdf->Image('_fonts/img/batismo.png',0,0,222); //primeiro valor posição em X,segundo valor posição em Y, terceiro valor tamanho
 $pdf->Image('_fonts/img/batismo.png',0,147,222); //primeiro valor posição em X,segundo valor posição em Y, terceiro valor tamanho
-$pdf->Text(80, 64.5, "Bruno do Nascimento Orosco");
-$pdf->Text(80, 80, " XX de Mês de Ano");
-$pdf->Text(80, 64.5+147, "Teste nome de membro");
-$pdf->Text(80, 74.5+147, " XX de Mês de Ano");
+$pdf->Text(80, 64.5, "Jose Carlos Alves da Silva");
+$pdf->Text(80, 64.5+147, "Michele Alves da Silva");
+
+$pdf->SetFont('Arial', '', 18);
+$pdf->SetTextColor(10,10,10); //rGB
+$pdf->Text(110, 76.5, "26 de Junho de 2011");
+$pdf->Text(110, 76.5+147, "21 de Julho de 2013");
 
 $pdf->Output();
 
